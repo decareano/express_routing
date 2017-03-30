be4e2546ddfe7d44055a009a3509e400fa11c544
Macadmin Local <macadminlocal@m-w80164tfagx.bmrn.com>
2017-03-30 16:41:24 -0700

    sequelize-initial

---
+++ server.js
@@ -0,0 +1,30 @@
+var Sequelize = require('sequelize');
+
+
+var sequelize = new Sequelize('database', 'firstName', 'lastName', {
+  host: 'localhost',
+  dialect: 'postgres' ,
+
+
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+
+  // SQLite only
+  //storage: 'path/to/database.sqlite'
+});
+
+// Or you can simply use a connection uri
+//var sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname');
+
+sequelize
+  .authenticate()
+  .then(function(err) {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(function (err) {
+    console.log('Unable to connect to the database:', err);
+  });
+
